a41a880090c934e244cf23d3904f5363d50fa71c
cami670v <cami670v@ZBC-AA-UBUY12>
2020-08-03 13:40:16 +0200

    Add project files.

---
+++ Pythagoras/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pythagoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2);
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
